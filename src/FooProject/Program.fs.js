8e9969547feab5ff28ac0e926fb9bbe41733fef5
Kevin Barabash <kevinb7@gmail.com>
2023-10-21 14:06:51 -0400

    rebuild project, add LICENSE and README.md files

--- src/FooProject/Program.fs.js
+++ src/FooProject/Program.fs.js
@@ -1,8 +1,8 @@
 import { printf, toConsole } from "./fable_modules/fable-library.4.3.0/String.js";
-import { hello } from "../FooLib/Library.fs.js";
+import { Say_hello } from "../FooLib/Library.fs.js";
 
 toConsole(printf("Hello from F#"));
 
-hello("F#");
+Say_hello("F#");
 
 //# sourceMappingURL=Program.fs.js.map
